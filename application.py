4b05de63c03d3780d935fa3d12d93944828de86a
snhu-cs-22 <110997198+snhu-cs-22@users.noreply.github.com>
2023-05-14 09:10:27 -0400

    Split main.py into application and main_window

---
+++ application.py
@@ -0,0 +1,186 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QStandardPaths, QTimer, QTime, pyqtSignal, pyqtSlot
+from PyQt5.QtGui import QKeySequence
+from PyQt5.QtWidgets import QApplication, QFileDialog, QMessageBox, QShortcut
+
+from main_window import MainWindow
+from plan import Plan
+from tasklist import Tasklist
+
+class Application(QApplication):
+    PATH_APPDATA = QStandardPaths.writableLocation(QStandardPaths.AppDataLocation) + "/LibrePlan"
+    PATH_ACTIVITY_DATA = PATH_APPDATA + "/plan.json"
+    PATH_TASK_DATA = PATH_APPDATA + "/tasklist.json"
+
+    countdownUpdateRequested = pyqtSignal(int)
+    titleUpdateRequested = pyqtSignal()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.plan = Plan()
+        self.tasklist = Tasklist()
+        self.load_data()
+
+        self.timer_activity_end = QTimer()
+        self.timer_activity_end.setSingleShot(True)
+        self.timer_countdown = QTimer()
+
+        self.main_window = MainWindow(self)
+
+        self._connectSignals()
+        self._connectSlots()
+        self.main_window.show()
+
+    # State management
+    ################################################################################
+
+    def load_data(self):
+        self.plan.import_activities(self.PATH_ACTIVITY_DATA)
+        self.tasklist.import_tasks(self.PATH_TASK_DATA)
+
+    def save_data(self):
+        self.plan.export_activities(self.PATH_ACTIVITY_DATA)
+        self.tasklist.export_tasks(self.PATH_TASK_DATA)
+
+    # Qt Slots/Signals
+    ################################################################################
+
+    def _connectSignals(self):
+        # Timers
+        self.timer_activity_end.timeout.connect(self.activity_end_dialog)
+        self.timer_countdown.timeout.connect(self.send_window_title_update_signal)
+
+    def _connectSlots(self):
+        # Main window
+        self.main_window.aboutDialogRequested.connect(self.show_about_dialog)
+        self.main_window.planImportRequested.connect(self.import_plan_dialog)
+        self.main_window.planExportRequested.connect(self.export_plan_dialog)
+        self.main_window.tasklistImportRequested.connect(self.import_tasklist_dialog)
+        self.main_window.tasklistExportRequested.connect(self.export_tasklist_dialog)
+
+        self.main_window.planStartRequested.connect(self.plan_start)
+        self.main_window.planEndRequested.connect(self.plan_end)
+        self.main_window.planInterruptRequested.connect(self.plan_interrupt)
+        self.main_window.planAbortRequested.connect(self.plan_abort)
+
+        self.main_window.appExitRequested.connect(self.exit_app)
+
+    # Dialogs
+    ################################################################################
+
+    def show_about_dialog(self):
+        text = (
+            "<center>"
+            "<h1>LibrePlan</h1>"
+            "<p>A simple, free and open-source day planner and to-do list.</p>"
+            "<p>Based on Plan and Tasklist Manager from SuperMemo.</p>"
+            "</center>"
+        )
+        QMessageBox.about(self.main_window, "About LibrePlan", text)
+
+    def activity_end_dialog(self):
+        """Alert user that the activity is ended"""
+
+        # Bring main window to front
+        self.main_window.setWindowState(self.main_window.windowState() & ~QtCore.Qt.WindowMinimized | QtCore.Qt.WindowActive)
+        self.main_window.activateWindow()
+
+        self.alert(self.main_window)
+        dialog = QMessageBox.question(self.main_window, "End of activity", "Time's up for activity: [PLACEHOLDER], end now?")
+
+        if dialog == QMessageBox.Yes:
+            self.plan_end()
+
+    def import_tasklist_dialog(self):
+        path = QFileDialog.getOpenFileName(None, "Import Tasks")[0]
+
+        if path:
+            if self.tasklist.tasks:
+                replace = QMessageBox.warning(
+                            None, None,
+                            "There are tasks currently in this list. Replace?",
+                            QMessageBox.Yes | QMessageBox.No | QMessageBox.Cancel
+                        )
+
+                if replace == QMessageBox.Yes:
+                    self.tasklist.import_tasks(path, True)
+                elif replace == QMessageBox.No:
+                    self.tasklist.import_tasks(path, False)
+            else:
+                self.tasklist.import_tasks(path)
+
+    def export_tasklist_dialog(self):
+        path = QFileDialog.getSaveFileName(None, "Export Tasks")[0]
+
+        if path:
+            self.tasklist.export_tasks(path)
+
+    def import_plan_dialog(self):
+        path = QFileDialog.getOpenFileName(None, "Import Plan")[0]
+
+        if path:
+            if self.plan.activities:
+                replace = QMessageBox.warning(
+                            None, None,
+                            "There are activities in the current plan. Replace?",
+                            QMessageBox.Yes | QMessageBox.No | QMessageBox.Cancel
+                        )
+
+                if replace == QMessageBox.Yes:
+                    self.plan.import_activities(path, True)
+                elif replace == QMessageBox.No:
+                    self.plan.import_activities(path, False)
+            else:
+                self.plan.import_activities(path)
+
+    def export_plan_dialog(self):
+        path = QFileDialog.getSaveFileName(None, "Export Plan")[0]
+
+        if path:
+            self.plan.export_activities(path)
+
+    # Program functions
+    ################################################################################
+
+    def send_window_title_update_signal(self):
+        if self.timer_countdown.isActive():
+            self.countdownUpdateRequested.emit(self._time_remaining())
+        else:
+            self.titleUpdateRequested.emit()
+
+    def _time_remaining(self):
+        return QTime().currentTime().secsTo(self.countdown_to)
+
+    def plan_start(self):
+        print("Activity started")
+
+        now = QTime.currentTime()
+        self.countdown_to = self.plan.activities[1].start_time
+        self.timer_activity_end.start(now.msecsTo(self.countdown_to))
+        self.timer_countdown.start(490)
+
+        self.send_window_title_update_signal()
+
+    def plan_end(self):
+        print("Activity ended")
+        self.timer_countdown.stop()
+        self.timer_activity_end.stop()
+        self.send_window_title_update_signal()
+
+    def plan_interrupt(self):
+        print("Activity interrupted")
+
+    def plan_abort(self):
+        print("Activity aborted")
+        self.timer_countdown.stop()
+        self.timer_activity_end.stop()
+        self.send_window_title_update_signal()
+
+    # App exit
+    ################################################################################
+
+    def exit_app(self):
+        self.save_data()
+        print("Program exited successfully.")
+        super().exit(0)
---
+++ main_window.py
@@ -0,0 +1,143 @@
+from typing import Callable, Sequence, Tuple, List
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QTime, pyqtSignal, pyqtSlot
+from PyQt5.QtGui import QKeySequence
+from PyQt5.QtWidgets import QApplication, QMainWindow, QShortcut
+
+from ui.forms.main_window import Ui_MainWindow
+from item_delegates import GenericDelegate, DeadlineTypeDelegate
+from plan import Plan
+from plan_table import PlanTableModel
+from tasklist import Tasklist
+from tasklist_table import TasklistTableModel
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    aboutDialogRequested = pyqtSignal()
+    planImportRequested = pyqtSignal()
+    planExportRequested = pyqtSignal()
+    tasklistImportRequested = pyqtSignal()
+    tasklistExportRequested = pyqtSignal()
+
+    planStartRequested = pyqtSignal()
+    planEndRequested = pyqtSignal()
+    planInterruptRequested = pyqtSignal()
+    planAbortRequested = pyqtSignal()
+
+    appExitRequested = pyqtSignal()
+
+    def __init__(self, application, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.application = application
+        self.setupUi(self)
+        self._connectSignals()
+        self._connectSlots()
+        self._setupKeys()
+
+        self._set_up_tables(application.plan, application.tasklist)
+
+    # Qt Slots/Signals
+    ################################################################################
+
+    def _connectSignals(self):
+        # Menu Actions
+        self.actionAbout.triggered.connect(self.aboutDialogRequested)
+        self.actionExit.triggered.connect(self.appExitRequested)
+        self.actionImport_Tasklist.triggered.connect(self.tasklistImportRequested)
+        self.actionExport_Tasklist.triggered.connect(self.tasklistExportRequested)
+        self.actionImport_Plan.triggered.connect(self.planImportRequested)
+        self.actionExport_Plan.triggered.connect(self.planExportRequested)
+
+        # Button Actions
+        self.pushButton_start.clicked.connect(self.planStartRequested)
+        self.pushButton_end.clicked.connect(self.planEndRequested)
+        self.pushButton_interrupt.clicked.connect(self.planInterruptRequested)
+        self.pushButton_abort.clicked.connect(self.planAbortRequested)
+
+        # Other widgets
+        self.tasklist_filter.textEdited.connect(self.filter_tasklist)
+
+    def _connectSlots(self):
+        self.application.countdownUpdateRequested.connect(self.update_title_countdown)
+        self.application.titleUpdateRequested.connect(self.update_title)
+
+    def _setupKeys(self):
+        globalShortcuts = [
+            ("Ctrl+R", self.planStartRequested.emit),
+            ("Ctrl+E", self.planEndRequested.emit),
+            ("Ctrl+I", self.planInterruptRequested.emit),
+            ("Ctrl+B", self.planAbortRequested.emit),
+
+            ("Ctrl+Tab", lambda: self.cycle_tabs(1)),
+            ("Ctrl+Shift+Tab", lambda: self.cycle_tabs(-1)),
+        ]
+        self._applyShortcuts(globalShortcuts)
+
+    def _applyShortcuts(
+        self, shortcuts: Sequence[Tuple[str, Callable]]
+    ) -> List[QShortcut]:
+        qshortcuts = []
+        for key, fn in shortcuts:
+            scut = QShortcut(QKeySequence(key), self, activated=fn)  # type: ignore
+            scut.setAutoRepeat(False)
+            qshortcuts.append(scut)
+        return qshortcuts
+
+    # GUI stuff
+    ################################################################################
+
+    def cycle_tabs(self, ahead):
+        tab_count = self.tabWidget.count()
+        next_index = (self.tabWidget.currentIndex() + ahead) % tab_count
+        self.tabWidget.setCurrentIndex(next_index)
+
+    def _set_up_tables(self, plan, tasklist):
+        # Plan
+
+        headers = ["F", "R", "Start", "Name", "Length", "ActLen", "OptLen", "Percent"]
+        self.plan_model = PlanTableModel(self, plan, headers)
+        self.plan_model.dataChanged.connect(self.update_title)
+        self.table_plan.setModel(self.plan_model)
+
+        self.plan_delegate = GenericDelegate(self)
+        self.table_plan.setItemDelegate(self.plan_delegate)
+
+        self.table_plan.resizeColumnsToContents()
+        self.table_plan.horizontalHeader().setSectionsMovable(True)
+
+        # Tasklist
+
+        headers = ["Priority", "Value", "Cost", "Name", "Date Added", "Deadline", "Halftime", "Deadline Type"]
+        self.tasklist_model = TasklistTableModel(self, tasklist, headers)
+        self.tasklist_model.dataChanged.connect(self.update_title)
+        self.table_tasklist.setModel(self.tasklist_model)
+
+        tasklist_delegate = GenericDelegate(self)
+        deadline_type_delegate = DeadlineTypeDelegate(self)
+        self.table_tasklist.setItemDelegate(tasklist_delegate)
+        self.table_tasklist.setItemDelegateForColumn(7, deadline_type_delegate)
+
+        self.table_tasklist.resizeColumnsToContents()
+        self.table_tasklist.horizontalHeader().setSectionsMovable(True)
+
+        self.update_title()
+
+    def filter_tasklist(self, query):
+        print("TODO: Implement tasklist filtering functionality")
+        print(f"Text query: {query}")
+
+    def update_title(self):
+        activity_count = self.plan_model.rowCount(None) - 1
+        task_count = self.tasklist_model.rowCount(None)
+
+        self.setWindowTitle(f"{activity_count} Activities, {task_count} Tasks - LibrePlan")
+
+    def update_title_countdown(self, secs_to):
+        self.setWindowTitle(f"{self._stringify_time_duration(secs_to)} - LibrePlan")
+
+    def _stringify_time_duration(self, secs, time_format="hh:mm:ss"):
+        """Workaround for having no idea how to properly represent time *durations*"""
+
+        if secs >= 0:
+            return QTime(0,0,0).addSecs(secs).toString(f"{time_format}")
+        return QTime(0,0,0).addSecs(-secs).toString(f"-{time_format}")
