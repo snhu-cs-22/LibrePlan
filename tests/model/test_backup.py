25ddaea6358ce4ae1536917324a7a169494fc1f6
snhu-cs-22 <110997198+snhu-cs-22@users.noreply.github.com>
2024-05-02 10:51:16 -0400

    Add backup creation/restoration test

--- tests/model/test_backup.py
+++ tests/model/test_backup.py
@@ -17,6 +17,23 @@ def backup_path(tmp_path):
 def backup(backup_path, database, config):
     return Backup(backup_path, database, config)
 
+def test_creation_restoration(tmp_path, backup, database, config):
+    new_key = "new_key"
+    old_key = "old_key"
+    backup_path = str(tmp_path / "backups" / "collection.db")
+
+    config.set_setting(old_key, 123)
+    backup.export(backup_path)
+    config.set_setting(new_key, 123)
+    backup.restore(backup_path)
+
+    new_database = Database(str(tmp_path / "test.db"))
+    assert new_database.connect()
+
+    new_config = Config(new_database)
+    assert new_config.has_setting(old_key)
+    assert not new_config.has_setting(new_key)
+
 def test_backup_rotation(backup, backup_path, config):
     backup_dir = QDir(backup_path)
     backup_dir.setFilter(QDir.Files)
